e447ad806d7b2285f785d79e534a3e1e48fb0d92
olalaz13 <phuocc281206@gmail.com>
2026-04-02 12:34:13 +0700

    Final MongoDB Atlas URI update

--- back-end/src/config/db.js
+++ back-end/src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/clothes-shop');
+        const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://phuocc281206_db_user:hihi@cluster0.3distpc.mongodb.net/clothes-shop?retryWrites=true&w=majority');
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (err) {
         console.error(`MongoDB connection error: ${err.message}`);
